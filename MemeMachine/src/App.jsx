aef22f9c966cd30d73d6f4520ff169198c41c801
Pavan Mistry <pavan32100@gmail.com>
2024-05-22 02:59:17 -0400

    Initial commit

---
+++ MemeMachine/src/App.jsx
@@ -0,0 +1,13 @@
+import MemeMachine from "./MemeMachine";
+import "./assets/css/styles.css";
+
+const App = () => {
+  return (
+    <main id="main">
+      {/* <h1>MemeMachine</h1> */}
+      <MemeMachine />
+    </main>
+  );
+};
+
+export default App;
---
+++ MemeMachine/src/MemeMachine.jsx
@@ -0,0 +1,69 @@
+import React, { useState, useEffect } from "react";
+import axios from "axios";
+
+const MemeMachine = () => {
+  const [meme, setMeme] = useState(null);
+  const [memeTitle, setMemeTitle] = useState(null);
+  const [loading, setLoading] = useState(true);
+  const [accessToken, setAccessToken] = useState(null);
+
+  const refreshAccessToken = async () => {
+    try {
+      const tokenResponse = await axios.post("https://www.reddit.com/api/v1/access_token", null, {
+        auth: {
+          username: import.meta.env.VITE_CLIENT_ID,
+          password: import.meta.env.VITE_CLIENT_SECRET,
+        },
+        params: {
+          grant_type: "client_credentials",
+        },
+      });
+      setAccessToken(tokenResponse.data.access_token);
+    } catch (error) {
+      console.error("Error refreshing access token: ", error);
+      throw error;
+    }
+  };
+  const fetchMeme = async () => {
+    setLoading(true);
+    try {
+      if (!accessToken) {
+        await refreshAccessToken();
+      }
+      const response = await axios.get("https://oauth.reddit.com/r/ProgrammerHumor/random", {
+        headers: {
+          Authorization: `Bearer ${accessToken}`,
+        },
+      });
+
+      const memeUrl = response.data[0].data.children[0].data.url;
+      setMeme(memeUrl);
+    } catch (error) {
+      console.error("Error fetching meme: ", error);
+    } finally {
+      setLoading(false);
+    }
+  };
+
+  useEffect(() => {
+    fetchMeme();
+  }, []);
+
+  return (
+    <div className="content-wrapper">
+      {loading ? (
+        <section className="main-content">
+          <p>Loading...</p>
+        </section>
+      ) : (
+        <section className="main-content">
+          <h2>{memeTitle}</h2>
+          <img src={meme} alt={memeTitle} className="meme-image" />
+          <button onClick={fetchMeme}>Refresh Meme</button>
+        </section>
+      )}
+    </div>
+  );
+};
+
+export default MemeMachine;
